d610f04a78510383e6278065d7a61fdfe0b8d138
rohanpathak2000 <75062006+rohanpathak2000@users.noreply.github.com>
2022-04-08 23:58:32 +0530

    Minor Changes

--- webApp/Users/views.py
+++ webApp/Users/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User,auth
 # Create your views here.
 
 def register(request):
-    ## Register code
-    ## If username already exists
-        ## Redirect back to signin with message
-    ## else
-        ## Redirect to home
-
+    if request.method == 'POST':
+        username = request.POST['uname']
+        pwd = request.POST['psw']
+        email = request.POST['email']
+        fname = request.POST['first_name']
+        lname = request.POST['last_name']
+        user = User.objects.create_user(username = uname,
+                                        
+                                        )
     return render(request,'register.html')
 
 
