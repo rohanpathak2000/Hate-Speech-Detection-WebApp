962faa6546a832f9ce918f3b28807afbdfe2ea83
utkarsh2yadav <utkarshyadav256@gmail.com>
2022-04-10 23:49:49 +0530

    Minor Changes

--- webApp/Users/views.py
+++ webApp/Users/views.py
@@ -1,9 +1,10 @@
+from contextlib import redirect_stderr
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 # Create your views here.
 from django.contrib import messages
 
-def register(request):
+def registerUser(request):
     if request.method == 'POST':
         username = request.POST['uname']
         pwd = request.POST['psw']
@@ -13,11 +14,11 @@ def register(request):
         if User.objects.filter(username = username).exists():
             print("Username taken")
             messages.info(request,'Username already taken')
-            return redirect('register')
+            return redirect('/register')
         elif User.objects.filter(email = email).exists():
             print("Email taken")
             messages.info(request,'Email Taken')
-            return redirect('register')
+            return redirect('/register')
         else:
             user = User.objects.create_user(username = username,
                                         password = pwd,
@@ -42,11 +43,16 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             return redirect('/')
+        else:
+            messages.info(request,'Username or Password incorrect')
+            return redirect('/login')
     else:
         return render(request,'signin.html')
 
-def home(request):
-    return render(request,'index.html')
 
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 
 
--- webApp/webApp/urls.py
+++ webApp/webApp/urls.py
@@ -1,28 +1,18 @@
-"""webApp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from detection.views import storeValues
-from Users.views import register,login,home
+from Users.views import registerUser,login,logout
+from detection.views import home
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('connect/',mainMethod),
     path('initialize/',storeValues),
-    path('register/',register),
+    path('register/',registerUser),
     path('login/',login),
+    path('logout/',logout),
+
     path('',home)
 
 ]
